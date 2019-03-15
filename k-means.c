24058d3aa833c63148219423de7ec72526ca316e
Michael-Gkotsis <mixalis.gotsis@gmail.com>
2019-03-15 12:13:44 +0200

    Adding currently best working versions

--- k-means.c
+++ k-means.c
@@ -1,152 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include "FileHandling.h"
+#include <math.h>
 #include <string.h>
+#include <time.h>
+
 
 int main(int argc, char *argv[])
 {
 
-    int n = 0;          // n Elements                // return 0 --> programm ended
-    int k;              // k clusters                                                                  // return -1 --> problem with the given file
-    int i, j;           // i counter for n, j counter for k
-    int d;              // d counter for dimensions
-    char fileName[100]; // The Variable for loading Custom Dataset
-    int dim = 0;        // Dimensions of Elements
-    char c;             // Temporary character for file analysis
-    int a = 1;          // Use a = 1 to generate random Centroids
-		int iteration = 0;  // Amount of repetitions
-		int flag_end = 0;   // Flag for ending k-means
-		int flag = -1;      // Flag for filtering
- srand(time(NULL));
+  int n = 0;           // n Elements  // For all loops always i < n !!!WARNING SOS!!!                // return 0 --> programm ended
+      int k;               // k clusters                                                               // return -1 --> problem with the given file
+      int i, j, d;         // i counter for n, j counter for k, d counter for dimensions               // return -2 --> problem with the initial file
+      char filename[100];  // The Variable for loading Custom Dataset                                  // return -3 --> problem with the error file
+      int dim = 0;         // Dimensions of Elements                                                   // return -4 --> problem with the error file 2.0
+      char c;              // Temporary character for file analysis                                    // return -5 --> problem with the final file
+      int a = 1;           // Use a = 1 to generate random Centroids                                   // return -6 --> problem with the result file
+  		int iteration = 0;   // Amount of repetitions
+  		int flag_end = 0;    // Flag for ending k-means
+  		int flag = -1;       // Flag for filtering
+      clock_t start, end;  // Count the time
+  int choise;// Choise holder for switch
+
+
 /* -------------------------------------------------------------------------- */
-    // Reading Dataset, counting n elements, assigning to X Array
+    // Reading Dataset
 
-    FILE* Dataset;
+   FILE* Dataset;
 
-       printf("\n Give the DataSet file:");
-       scanf("%s", fileName);
+      printf("\n Give the DataSet file:");
+      scanf("%s", filename);
 
-    Dataset = fopen(fileName, "r");
+    Dataset = fopen(filename, "r");
     if (!Dataset)
     {
-        printf("\n There is something wrong with the file! \n\n");
+        printf("\n There is something wrong with the Dataset file! \n\n");
         return -1;
     }
+
 /* -------------------------------------------------------------------------- */
-    // Finding the dimensions of Elements
-
-    do
-    {
-        fscanf(Dataset, "%c", &c);
-        if (c == ',')
-            dim++;
-    } while (c != '\n');
-       dim++;
-    rewind(Dataset); // File reset
 
 
-    // Finding the number of elements
 
-    do
-    {
-        fscanf(Dataset, "%c", &c);
-        if (c == '\n')
-            n++;
-    } while (!feof(Dataset));
 
-    rewind(Dataset); // File reset
-  n--;
-    printf("\n Elements:%d \n", n);
+
+    dim = getColumns(Dataset); //Getting the dimensions of each Element of the Dataset
+    rewind(Dataset);
+
+    n = getRows(Dataset);       // Getting the Elements of the Dataset
+    rewind(Dataset);
+
+    printf("\n Elements:%d \n", n-1);
     printf("\n Dimensions:%d \n", dim);
 
-/* -------------------------------------------------------------------------- */
     printf("\n Choose the amount of Clusters:");
     scanf("%d", &k);
-/* -------------------------------------------------------------------------- */
-    // All the necessary memory allocation
 
-    double **X;  // Array of Elements
-    X = calloc(n, sizeof(double));
-    for (d = 0; d < n; d++)
+/* -------------------------------------------------------------------------- */
+        // All the necessary memory allocation
+
+        double **X;   // Array of Elements
+        X = calloc(n, sizeof(double));
+        for (d = 0; d < n; d++)
         X[d] = calloc(dim, sizeof(double));
 
-    double **V;  // Array of Centroids
-    V = calloc(k, sizeof(double));
-    for (d = 0; d < k; d++)
-        V[d] = calloc(dim, sizeof(double));
+        double **V;  // Array of Centroids
+            V = calloc(k, sizeof(double));
+            for (d = 0; d < k; d++)
+                V[d] = calloc(dim, sizeof(double));
 
-    double **C;  // Array of Clusters                                            //C[n][dim+1] --> C[n][0] always stands for j
-    C = calloc(k * (n), sizeof(double));
-    for (d = 0; d <= k*(n); d++)
-        C[d] = calloc((dim+1), sizeof(double));
+                double **FlagCentroids;  // Array of flag Centroids
+            FlagCentroids = calloc(k, sizeof(double));
+            for (d = 0; d < k; d++)
+                FlagCentroids[d] = calloc(dim, sizeof(double));
 
-    double **FlagCentroids;  // Array of flag Centroids
-    FlagCentroids = calloc(k, sizeof(double));
-    for (d = 0; d < k; d++)
-        FlagCentroids[d] = calloc(dim, sizeof(double));
+                int *counter;  // Counter of elements for each cluster
+            counter = calloc(k, sizeof(int));
 
-		int *counter;  // Counter of elements for each cluster
-    counter = calloc(k, sizeof(int));
+        		double **totalCluster;  // Sum of every element for each cluster
+            totalCluster= calloc(k,sizeof(double));
+        		for(d = 0; d < k; d++)
+        		   totalCluster[d] = calloc(dim,sizeof(double));
 
-		double **totalCluster;  // Sum of every element for each cluster
-    totalCluster= calloc(k,sizeof(double));
-		for(d = 0; d < k; d++)
-		   totalCluster[d] = calloc(dim,sizeof(double));
+        		double **distance;  // Array of Distance
+        		distance = calloc(n,sizeof(double));
+        			 for(j = 0; j < n; j++)
+        			 distance[j] = calloc(k,sizeof(double));
 
-		double **distance;  // Array of Distance
-		distance = calloc(n,sizeof(double));
-			 for(j = 0; j < n; j++)
-			 distance[j] = calloc(k,sizeof(double));
+            double *min;  // Array of minimum distance
+            min = calloc(n,sizeof(double));
 
-    double *min;  // Array of minimum distance
-    min = calloc(n,sizeof(double));
+            int *location;  // Array of Locations
+            location = calloc(n,sizeof(int));
 
-    int *location;  // Array of Locations
-    location = calloc(n,sizeof(int));
-
-    FILE** Cluster = calloc(k+1,sizeof(FILE*));
-
-    FILE** initialFile = calloc(k+1,sizeof(FILE*));
-
-    FILE** finalFile = calloc(k+1,sizeof(FILE*));
-
-    FILE* Centroids;
-
-
+            int *Cluster;  // Array of Clusters
+            Cluster = calloc(n,sizeof(int));
 
 
 /* -------------------------------------------------------------------------- */
-    // Passing elements to Array X[n][dim]
+                  // Passing elements to Array X[n][dim]
+                 n--;
 
 
-   while(!feof(Dataset))
-    for (i = 0;  i < n; i++)
-    {
-        for (d = 0; d < dim; d++)
-        {
-            fscanf(Dataset, "%lf,", &X[i][d]);
-        }
-    }
+                    X = getData(Dataset,n,dim,X);
+                    
 
 
-
-    fclose(Dataset);
+            fclose(Dataset);
+  start = clock();
+/* -------------------------------------------------------------------------- */
+/* ---------------------------------K-MEANS---------------------------------- */
+/* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
     // Generating Initial Random Centroids
 
     for (j = 0; j < k; j++)
     {
-      i = ((rand() % ((n) - a + 1)) + a);
+      i = rand() % n;
         for (d = 0; d < dim; d++)
             V[j][d] = X[i][d];
     }
-
-
-
-    
-/* -------------------------------------------------------------------------- */
-
-
 
 /* -------------------------------------------------------------------------- */
     // Assigning the initial random centroids to FlagCentroids
@@ -154,225 +127,179 @@ int main(int argc, char *argv[])
     for (j = 0; j < k; j++)
         for (d = 0; d < dim; d++)
             FlagCentroids[j][d] = V[j][d];
-/* -------------------------------------------------------------------------- */
 
 /* -------------------------------------------------------------------------- */
 
+do{
 
-    do {
 
+  //   Setting Sum and Counter of each cluster to 0
+  for(j = 0; j < k; j++)
+  {
+    for(d = 0; d < dim; d++)
+    {
+      totalCluster[j][d] = 0;
+      counter[j] = 0;
+    }
+  }
 
-/* -------------------------------------------------------------------------- */
-      //   Setting Sum and Counter of each cluster to 0
-
-          for(j = 0; j < k; j++)
-             for(d = 0; d < dim; d++)
-               {
-                 totalCluster[j][d] = 0;
-                 counter[j] = 0;
-               }
 /* -------------------------------------------------------------------------- */
   // Calculating Distances of each element for each cluster and each dimension
 
-          for(i = 0; i < n; i++)
-             for(j = 0; j < k; j++)
-               for(d = 0; d < dim; d++)
-             distance[i][j] += ((X[i][d] - V[j][d])*(X[i][d] - V[j][d]));
+  for(i = 0; i < n; i++)
+  {
+    for(j = 0; j < k; j++)
+    {
+      for(d = 0; d < dim; d++)
+      {
+      distance[i][j] += ((X[i][d] - V[j][d])*(X[i][d] - V[j][d]));
+      }
+      distance[i][j] = sqrt(distance[i][j]);
+    }
+  }
 
+
+/*-------------------------------------------------------------------------- */
 
 /* -------------------------------------------------------------------------- */
       // Comparing Distances and getting the proper Cluster for each Element
 
-      for(i=0;i < n;i++)
+      for(i = 0; i < n; i++)
       {
-          min[i]=distance[i][0];
-          for(j=0;j < k;j++)
+        min[i] = distance[i][0];
+        for(j = 0; j < k; j++)
+        {
+          if(distance[i][j] <= min[i])
           {
-              if(distance[i][j]<=min[i])
-              {
-                   min[i]=distance[i][j];
-                         location[i]=j;
-               }
+            min[i] = distance[i][j];
+            location[i] = j;
           }
+        }
       }
-
-
+/* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
       // Getting Elements and Clusters all together
 
       for(i = 0; i < n; i++)
-      C[i][0] = location[i];
-
-      for(i = 0; i < n; i++)
-         for(d = 1; d <= dim; d++)
-       C[i][d] = X[i][d-1];
-
-
+      Cluster[i] = location[i];
+/* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
       // Calculating total Sum of each dim for each cluster and counter
-
-       for(i = 0; i < n; i++)
-          for(j = 0; j < k; j++)
-             for(d = 0; d < dim; d++)
-              if(C[i][0] == j)
-              {
-              totalCluster[j][d] += C[i][d+1];
-              counter[j]++;
-              }
-
-
+      for(i = 0; i < n; i++)
+      {
+        for(j = 0; j < k; j++)
+        {
+          if(Cluster[i] == j)
+          {
+            for(d = 0; d < dim; d++)
+            {
+              totalCluster[j][d] += X[i][d];
+            }
+            counter[j]++;
+          }
+        }
+      }
+/* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
     // Calculating New Centroid
 
-              for(j = 0; j < k; j++)
-                  for(d = 0; d < dim; d++)
-                     V[j][d] = (totalCluster[j][d])/(counter[j]/dim);
-
-
+    for(j = 0; j < k; j++)
+    {
+      for(d = 0; d < dim; d++)
+      {
+        V[j][d] = totalCluster[j][d]/counter[j];
+      }
+    }
+/* -------------------------------------------------------------------------- */
 /* -------------------------------------------------------------------------- */
 			// Here goes the comparison last FlagCentroid with new centroids
-                for(j = 0; j < k; j++)
-                {
-                    for(d = 0; d < dim; d++)
-                    if(FlagCentroids[j][d] != V[j][d])
-                    {
-                        flag_end = 0;
-                        break;
-                    }else
-                    {
-                        flag_end = -1;
-                        break;
-                    }
-                 }
-/* -------------------------------------------------------------------------- */
-          // Passing New centroids to flag;
-                  for(j = 0; j < k; j++)
-                    for(d = 0; d < dim; d++)
-                    FlagCentroids[j][d] = V[j][d];
 
-                    iteration++;
-
-		} while(flag_end != -1);
-
-printf("\n\n Amount of Iterations: %d\n\n",iteration+1);
-/* -------------------------------------------------------------------------- */
-      // Writing to a file for Each cluster
-
-   for(j = 0 ; j < k ; j++)
-   {
-     char fileName [100];
-     sprintf(fileName,"Cluster_%d.csv",j);
-      Cluster[j] = fopen(fileName,"w");
-   }
-
-
-for(i = 0; i < n; i++){
-   for( j = 0; j < k; j++){
-      for(d = 1; d <= dim; d++){
-          if(C[i][0] == j){
-          fprintf(Cluster[j], "%lf",C[i][d]);
-          if(d <= dim - 1)
+      for(j = 0; j < k; j++)
+      {
+        for(d = 0; d < dim; d++)
+        {
+          if(FlagCentroids[j][d] != V[j][d])
           {
-            fprintf(Cluster[j],", ");
+            flag_end = 0;
+            break;
+          }else
+          {
+            flag_end = -1;
+            break;
           }
         }
-        }
- fprintf(Cluster[j], "\n" );
- }
-}
-
-for (j = 0; j < k; j++)
-fclose(Cluster[j]);
-
-
-//Creating and writing to a file the Final Centroids
-Centroids = fopen("FinalCentroids.csv","w");
-
-for(j = 0; j < k; j++){
-    if(j != 0)
-    fprintf(Centroids, "\n");
-   for(d = 0; d < dim; d++){
-   fprintf(Centroids, "%lf",FlagCentroids[j][d]);
-   if(d < dim -1){
-     fprintf(Centroids,", ");
-   }
- }
-}
-   fclose(Centroids);
+      }
 /* -------------------------------------------------------------------------- */
-       // Deleting Empty rows and Blank rows
+ // Passing New centroids to flag;
 
-int MAX = n*k*dim;
-char str[MAX];
-
-for(j = 0 ; j < k ; j++)
-{
-  char fileName [100] ;
-  sprintf(fileName,"Cluster_%d.csv",j);
-   initialFile[j] = fopen(fileName,"r");
-}
-
-for(j = 0 ; j < k ; j++)
-{
-  char fileName [100] ;
-  sprintf(fileName,"c%d.csv",j);
-   finalFile[j] = fopen(fileName,"w");
-}
-
-for(j = 0; j < k; j++)
-{
-   while(!feof(initialFile[j]))
+ for(j = 0; j < k; j++)
+ {
+   for(d = 0; d < dim; d++)
    {
-     fgets(str,MAX,initialFile[j]);
-     if(strcmp(str,"\n") == 0)
-     {
-       continue;
-     }
-     fputs(str,finalFile[j]);
-     strcpy(str, "\0");
+     FlagCentroids[j][d] = V[j][d];
    }
-}
+ }
+
+ iteration++;
 
 
-for(j = 0; j < k; j++)
-fclose(initialFile[j]);
+}while(flag_end != -1);
 
-for(j = 0; j < k; j++)
-fclose(finalFile[j]);
 /* -------------------------------------------------------------------------- */
-//Deleting Useless Files
+/* ---------------------------------END-------------------------------------- */
+/* -------------------------------------------------------------------------- */
+end = clock();
+/*----------------------------------------------------------------------------*/
+double total_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+printf("\n Time of Algorithm Execution: %lf \n\n",total_time);
+
+FILE** ClusterFile = calloc(k+1,sizeof(FILE*));
+
 for(j = 0 ; j < k ; j++)
+   {
+     char *fileName;
+    fileName = calloc(n,sizeof(char));
+     snprintf(fileName,n*sizeof(char),"C%d.txt",j);
+      ClusterFile[j] = fopen(fileName,"w");
+      free(fileName);
+   }
+
+for(j = 0; j < k; j++)
 {
-  char fileName [100] ;
-  sprintf(fileName,"Cluster_%d.csv",j);
-   remove(fileName);
+  for(i = 0; i < n; i++)
+  {
+    if(Cluster[i] == j)
+    {
+      for(d = 0; d < dim; d++)
+      {
+        fprintf(ClusterFile[j], "%lf ",X[i][d] );
+      }
+      fprintf(ClusterFile[j], "\n");
+    }
+  }
 }
-for(j = 20 ; j >= k ; j--)
+
+   for (j = 0; j < k; j++)
+   fclose(ClusterFile[j]);
+
+   for(j = 60 ; j >= k ; j--)
 {
-  char fileName [100] ;
-  sprintf(fileName,"c%d.csv",j);
+  char *fileName;
+  fileName = calloc(n,sizeof(char));
+  snprintf(fileName,n*sizeof(char),"C%d.txt",j);
    remove(fileName);
+   free(fileName);
 }
 
-
-
-/* -------------------------------------------------------------------------- */
-    // FREE EVERYTHING
-
-    free(X);
-    free(V);
-    free(C);
-    free(FlagCentroids);
-		free(counter);
-		free(totalCluster);
-		free(distance);
-    free(min);
-    free(location);
-    free(Cluster);
-    free(initialFile);
-    free(finalFile);
-
-
-    return 0;
+free(X);
+  free(V);
+  free(Cluster);
+free(FlagCentroids);
+  free(counter);
+  free(totalCluster);
+  free(distance);
+  free(min);
+  free(location);
+  free(ClusterFile);
+return 0;
 }
-
